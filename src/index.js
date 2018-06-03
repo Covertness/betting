9c9b34acc5b69d3b2e55d4d38b544c4a5e2c4617
Covertness <wuyingfengsui@gmail.com>
2018-06-03 20:35:42 +0800

    fix ie error

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// material-ui need it. https://stackoverflow.com/questions/26962341/number-isnan-doesnt-exist-in-ie
+Number.isNaN = (o) => typeof(o) === 'number' && isNaN(o);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
